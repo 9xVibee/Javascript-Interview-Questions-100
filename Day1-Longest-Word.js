aaf13f6a779c620e55fbc0dcb320c52046b79319
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-31 20:51:28 +0530

    chore: day 1 : Longest word in string

--- Day1-Longest-Word.js
+++ Day1-Longest-Word.js
@@ -1,1 +1,17 @@
 // Longest word in a string
+
+// ex: str = "demo example"
+// longest word = example
+
+let str = "demo example";
+
+let strArr = str.split(" ");
+let longestWord = "";
+
+for (let i = 0; i < strArr.length; i++) {
+  if (longestWord.length < strArr[i].length) {
+    longestWord = strArr[i];
+  }
+}
+
+console.log("Longest word is", longestWord);
