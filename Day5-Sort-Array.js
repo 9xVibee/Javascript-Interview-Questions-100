49af05e706b840122820b7930c8b66b6a1d4f57c
Abhay Panchal <abpanchal951357@gmail.com>
2024-04-05 08:22:58 +0530

    Day 5 : Sort the array

---
+++ Day5-Sort-Array.js
@@ -0,0 +1,8 @@
+// Sort the array
+
+const sortArrayInAscendingOrder = (arr) => {
+  return arr.sort((a, b) => a - b);
+};
+
+const res = sortArrayInAscendingOrder([5, 4, 3, 2, 1]);
+console.log(res);
